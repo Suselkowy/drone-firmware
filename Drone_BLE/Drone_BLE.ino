0145dd77a5a641b9c103fe67f7b1a811a74290ec
Suselkowy <jakubsus77@gmail.com>
2024-06-12 16:51:17 +0200

    add altitude

--- Drone_BLE/Drone_BLE.ino
+++ Drone_BLE/Drone_BLE.ino
@@ -8,8 +8,15 @@ BLEIntCharacteristic pulseCharacteristic("19b10010-e8f2-537e-4f6c-d104768a1214",
 
 #include "Wire.h"
 #include <MPU6050_light.h>
+#include <Adafruit_BMP280.h>
+
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
 
 MPU6050 mpu(Wire);
+Adafruit_BMP280 bmp; 
 unsigned long timer = 0;
 unsigned long pulseTimer = 0;
 
@@ -141,6 +148,27 @@ void setup() {
   Serial.begin(9600);
   Wire.begin();
 
+  unsigned bmpStatus;
+  //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+  bmpStatus = bmp.begin(0x76);
+  if (!bmpStatus) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
   byte status = mpu.begin();
   Serial.print(F("MPU6050 status: "));
   Serial.println(status);
@@ -191,6 +219,7 @@ void loop() {
     // Serial.print(mpu.getAngleY());
     // Serial.print("\tZ : ");
     // Serial.println(mpu.getAngleZ());
+    Serial.println(bmp.readAltitude(1013.25));
     timer = millis();  
     if( state == FLY ) {
       stablize();
